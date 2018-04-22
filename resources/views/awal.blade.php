1ea9a7bf3f8b991d069cc19acf36f6960f340cd5
andrdmynti <andrd.dmynti22@gmail.com>
2018-04-23 01:01:59 +0700

    edit views

---
+++ resources/views/awal.blade.php
@@ -0,0 +1,10 @@
+@extends('index')
+
+@section('content')
+<div class="content-wrapper">
+    <div class="row">
+      <div class="col-sm-1"></div>
+      <div class="col-sm-8">INI ADALAH ISI DARI KONTEN</div>
+    </div>
+  </div>
+@endsection
